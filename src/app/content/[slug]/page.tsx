ea91c1383f20669fd9334d98ac6bf8467d33ff5a
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-02-18 20:23:35 +0900

    add syntax highlight

--- src/app/content/[slug]/page.tsx
+++ src/app/content/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { Toc } from '@/app/_components/Toc';
 import { Content } from '@/app/types';
 import { client } from '@/lib/microcms';
+import { load } from 'cheerio';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/night-owl.css';
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const post: Content = await client.get({ endpoint: 'blog', contentId: params.slug });
@@ -8,6 +11,13 @@ export default async function Home({ params }: { params: { slug: string } }) {
   const publishedAtString = `${publishedAt.getFullYear()}年${publishedAt.getMonth() + 1}月${publishedAt.getDate()}日`;
   const revisedAt = new Date(post.revisedAt);
   const revisedAtString = `${revisedAt.getFullYear()}年${revisedAt.getMonth() + 1}月${revisedAt.getDate()}日`;
+  const $ = load(post.content);
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+  console.log($.html());
 
   return (
     <main>
@@ -21,7 +31,7 @@ export default async function Home({ params }: { params: { slug: string } }) {
       </div>
       <div
         dangerouslySetInnerHTML={{
-          __html: `${post.content}`,
+          __html: `${$.html()}`,
         }}
       />
       <div>
